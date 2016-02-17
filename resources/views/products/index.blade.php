cd20e6017cfebba604501e1dfa6f04b4739b9c78
Austin Ducworth <aducworth@gmail.com>
2016-02-17 15:49:50 -0500

    separating out individual add/edit product view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,95 +3,9 @@
 @section('content')
 	<div class="container">
 		<div class="col-sm-offset-2 col-sm-8">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					New Product
-				</div>
-
-				<div class="panel-body">
-					<!-- Display Validation Errors -->
-					@include('common.errors')
-
-					<form action="/product" method="POST" class="form-horizontal">
-						{{ csrf_field() }}
-
-						<!-- Product Name -->
-						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label">Product</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="name" id="task-name" class="form-control" value="{{ old('product') }}">
-							</div>
-						</div>
-						
-						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label">Purchase Price</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="purchase_price" id="task-name" class="form-control" value="{{ old('purchase_price') }}">
-							</div>
-						</div>
-						
-						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label">Sale Price</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="sale_price" id="task-name" class="form-control" value="{{ old('sale_price') }}">
-							</div>
-						</div>
-						
-						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label">Shipping Paid</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="shipping_paid" id="task-name" class="form-control" value="{{ old('shipping_paid') }}">
-							</div>
-						</div>
-						
-						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label">Actual Shipping</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="actual_shipping" id="task-name" class="form-control" value="{{ old('actual_shipping') }}">
-							</div>
-						</div>
-						
-						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label">Seller Fee</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="seller_fee" id="task-name" class="form-control" value="{{ old('seller_fee') }}">
-							</div>
-						</div>
-						
-						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label">Shipping Fee</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="shipping_fee" id="task-name" class="form-control" value="{{ old('shipping_fee') }}">
-							</div>
-						</div>
-						
-						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label">Transaction Fee</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="transaction_fee" id="task-name" class="form-control" value="{{ old('transaction_fee') }}">
-							</div>
-						</div>
-
-						<!-- Add Task Button -->
-						<div class="form-group">
-							<div class="col-sm-offset-3 col-sm-6">
-								<button type="submit" class="btn btn-default">
-									<i class="fa fa-btn fa-plus"></i>Add Product
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-
+			
+			@include('common.errors')
+			
 			<!-- Current Tasks -->
 			@if (count($products) > 0)
 				<div class="panel panel-default">
@@ -127,6 +41,8 @@
 						</table>
 					</div>
 				</div>
+			@else
+				<div class="alert alert-info" role="alert">No products currently in the database.</div>
 			@endif
 		</div>
 	</div>
