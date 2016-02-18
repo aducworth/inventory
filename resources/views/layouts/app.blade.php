7c9b74683b5151c8ebe3009958020bb3b001272a
Austin Ducworth <aducworth@gmail.com>
2016-02-18 07:08:41 -0500

    update app theme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,6 @@
 
 	<style>
 		body {
-			font-family: 'Raleway';
 			margin-top: 25px;
 		}
 		.fa-btn {
@@ -35,7 +34,7 @@
 
 <body>
 	<div class="container">
-		<nav class="navbar navbar">
+		<nav class="navbar navbar-inverse">
 			<div class="container-fluid">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -50,10 +49,9 @@
 
 				<div id="navbar" class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
-						<li><a href="/stores/"><i class="fa fa-btn fa-heart"></i>Stores</a></li>
-						<li><a href="/sources/"><i class="fa fa-btn fa-heart"></i>Sources</a></li>
-						<li><a href="/locations/"><i class="fa fa-btn fa-heart"></i>Locations</a></li>
-						<li><a href="/products/"><i class="fa fa-btn fa-heart"></i>Products</a></li>
+						@if (!Auth::guest())
+							<li><a href="/product/"><i class="fa fa-btn fa-heart"></i>Products</a></li>
+						@endif
 					</ul>
 
 					<ul class="nav navbar-nav navbar-right">
@@ -61,8 +59,17 @@
 							<li><a href="/auth/register"><i class="fa fa-btn fa-heart"></i>Register</a></li>
 							<li><a href="/auth/login"><i class="fa fa-btn fa-sign-in"></i>Login</a></li>
 						@else
-							<li class="navbar-text"><i class="fa fa-btn fa-user"></i>{{ Auth::user()->name }}</li>
-							<li><a href="/auth/logout"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+							<li role="presentation" class="dropdown">
+								<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+									{{ Auth::user()->name }} <span class="caret"></span>
+    							</a>
+    							<ul class="dropdown-menu">
+	    							<li><a href="/stores/"><i class="fa fa-btn fa-heart"></i>Stores</a></li>
+									<li><a href="/sources/"><i class="fa fa-btn fa-heart"></i>Sources</a></li>
+									<li><a href="/locations/"><i class="fa fa-btn fa-heart"></i>Locations</a></li>
+	    							<li><a href="/auth/logout"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+    							</ul>
+							</li>							
 						@endif
 					</ul>
 				</div>
